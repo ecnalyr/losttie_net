0fdc8fafd7a83b5633893f3210aabb1903fee16e
Ecnalyr <ecnalyr@gmail.com>
2013-09-20 09:53:15 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace losttie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WSDesignProcess()
+        {
+
+            return View();
+        }
+
+        public ActionResult Consultation()
+        {
+            return View();
+        }
+
+        public ActionResult BusinessSiteGoals()
+        {
+            return View();
+        }
+
+        public ActionResult WhoWeAre()
+        {
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult jQuery()
+        {
+            return View();
+        }
+
+        public ActionResult ResponsiveDesign()
+        {
+            return View();
+        }
+
+        public ActionResult ModernTech()
+        {
+            return View();
+        }
+
+        public ActionResult AppDevelopment()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="losttie.MvcApplication" Language="C#" %>
